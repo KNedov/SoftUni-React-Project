5eb187ead178c01f32bc6ca87de088c36272272d
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-11-30 16:38:41 +0200

    Fetch and display user's phone collection
    Integrates user context and custom request hook to fetch the logged-in user's phones. Renders ProductCard components for each phone or a NoPhoneMsg if none are found, replacing static cards with dynamic data.

--- client/src/components/my-products/MyProducts.jsx
+++ client/src/components/my-products/MyProducts.jsx
@@ -1,19 +1,30 @@
 import NoPhoneMsg from "../no-phone-msg/NoPhoneMsg";
 import ProductCard from "../product-card/ProductCard";
-import './MyProducts.css'
+import './MyProducts.css'     
+import {useUserContext} from "../../contexts/UserContext";
+import useRequest from "../../hooks/userRequest";
+
 
 export default function MyProducts() {
+    const{user}=useUserContext()
+const { data: myPhones } = useRequest(`/phones/my-phones/${user._id}`, [])
+
+
+
     return (
         <>
             <div className="my-phones">
                 <h2>My Phones Collection</h2>
                 <div className="container">
             <div className="phones-grid">
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+                {myPhones.length>0
+                ? myPhones.map((myPhone)=>{
+                    return <ProductCard key={myPhone._id} {...myPhone}/>
+                })
+                :<NoPhoneMsg/>
+                }
             </div>
-            <NoPhoneMsg/>
+            
             </div>
             </div>
             
