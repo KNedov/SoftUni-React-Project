5a13e89a2a93d8bda7b17a38e143402eb77aa5d6
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-12-09 19:22:33 +0200

    change createdAt sorting from desc to asc

--- Rest-api/controllers/commentController.js
+++ Rest-api/controllers/commentController.js
@@ -146,7 +146,7 @@ function deleteComment(req, res, next) {
             return commentModel
                 .find({ phoneId })
                 .populate("userId", "username")
-                .sort({ createdAt: 1 });
+                .sort({ createdAt: -1 });
         })
         .then((updatedComments) => {
             updatedComments.sort(
