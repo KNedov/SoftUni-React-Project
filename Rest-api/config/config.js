a87ca5d8870474240fd77d2c398aea2088855c8c
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-12-10 12:56:54 +0200

    remove / from Url in apiConfig

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -4,12 +4,12 @@ const config = {
     development: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb://localhost:27017/store-zone',
-        origin: ['http://localhost:5555', 'http://localhost:4200','http://localhost:5173','http://localhost:4173','https://softuni-react-project-1.onrender.com/']
+        origin: ['http://localhost:5555', 'http://localhost:4200','http://localhost:5173','http://localhost:4173','https://softuni-react-project-1.onrender.com']
     },
     production: {
         port: process.env.PORT || 3000,
         dbURL: process.env.DB_URL_CREDENTIALS,
-        origin: ['https://phonezone.onrender.com','http://localhost:5173','http://localhost:4173','https://softuni-react-project-1.onrender.com/']
+        origin: ['https://phonezone.onrender.com','http://localhost:5173','http://localhost:4173','https://softuni-react-project-1.onrender.com']
     }
 };
 
